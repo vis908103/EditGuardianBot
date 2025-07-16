fee3e410ae65e4e1978f83c61968a76e525e0bfe
vis908103 <kv2057279@gmail.com>
2025-07-16 17:04:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telegram import Update, Bot, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from config import TOKEN, OWNER_ID
+from config import 8081003333:AAEUgbL5JOJP4r294D4kUtrBU5fqTwegmjk, 1008989961
 
 def start(update: Update, context: CallbackContext):
     user = update.effective_user
