01ca1df638f2e47617cc0085b84706ab33235b0d
vis908103 <kv2057279@gmail.com>
2025-07-16 17:11:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = '8081003333:AAEUgbL5JOJP4r294D4kUtrBU5fqTwegmjk' # Get Your Telegram Bot Token from @BotFather
-OWNER_ID = '1008989961' # Add Here your telegram id 
+TOKEN = "8081003333:AAEUgbL5JOJP4r294D4kUtrBU5fqTwegmjk" # Get Your Telegram Bot Token from @BotFather
+OWNER_ID = "1008989961" # Add Here your telegram id 
